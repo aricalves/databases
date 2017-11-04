2993d5f609589f6c595a341196543b319d981c1a
aricalves <aric.alves2012@gmail.com>
2017-11-03 17:17:21 -0700

    Set up database

--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,47 @@
+DROP DATABASE IF EXISTS `chat`;
+
 CREATE DATABASE chat;
 
 USE chat;
-
-CREATE TABLE messages (
-  /* Describe your table here.*/
-);
-
-/* Create other tables and define schemas for them here! */
-
-
-
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
 
+-- ---
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+
+-- ---
+-- Table 'messages'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `messages`;
+    
+CREATE TABLE `messages` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `user` VARCHAR(20) NOT NULL,
+  `roomname` VARCHAR(20) NOT NULL,
+  `text` VARCHAR(200) NOT NULL,
+  `time_stamp` TIMESTAMP NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `messages` (`id`,`user`,`roomname`,`text`,`time_stamp`) VALUES
+-- ('','','','','');
